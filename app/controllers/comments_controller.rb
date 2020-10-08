6f023286e3efdfe3e3aae7ec2788aebddfd70f1c
techcamp830819 <odyssey3696@gmail.com>
2020-10-08 12:35:46 +0900

    commit

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,7 +3,7 @@ class CommentsController < ApplicationController
     comment = Comment.create(comment_params)
     redirect_to "/tweets/#{comment.tweet.id}" 
   end
-
+# aaa
   private
   def comment_params
     params.require(:comment).permit(:text).merge(user_id: current_user.id, tweet_id: params[:tweet_id])
